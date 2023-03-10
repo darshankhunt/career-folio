98b6af98223cda7e95f2519e51116c4f7ad8e431
Darshan Khunt <darshankhunt13@gmail.com>
2023-03-10 13:12:45 +0530

    Add Tab Pages and Icon

--- app/src/main/java/com/example/resumemaker/adapter/ResumeDetailsAdapter.java
+++ app/src/main/java/com/example/resumemaker/adapter/ResumeDetailsAdapter.java
@@ -8,7 +8,11 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.example.resumemaker.fragment.AboutFragment;
 import com.example.resumemaker.fragment.ContactFragment;
-import com.example.resumemaker.fragment.ThirdFragment;
+import com.example.resumemaker.fragment.DoneFragment;
+import com.example.resumemaker.fragment.EducationFragment;
+import com.example.resumemaker.fragment.SkillsFragment;
+import com.example.resumemaker.fragment.SummaryFragment;
+import com.example.resumemaker.fragment.WorkFragment;
 
 public class ResumeDetailsAdapter extends FragmentStateAdapter {
 
@@ -24,15 +28,21 @@ public class ResumeDetailsAdapter extends FragmentStateAdapter {
             case 1:
                 return  new ContactFragment();
             case 2:
-                return  new ThirdFragment();
+                return  new WorkFragment();
+            case 3:
+                return  new EducationFragment();
+            case 4:
+                return  new SkillsFragment();
+            case 5:
+                return  new SummaryFragment();
+            case 6:
+                return  new DoneFragment();
         }
-
-
         return new AboutFragment();
     }
 
     @Override
     public int getItemCount() {
-        return 6;
+        return 7;
     }
 }
--- app/src/main/java/com/example/resumemaker/fragment/DoneFragment.java
+++ app/src/main/java/com/example/resumemaker/fragment/DoneFragment.java
@@ -9,11 +9,11 @@ import androidx.fragment.app.Fragment;
 
 import com.example.resumemaker.R;
 
-public class ThirdFragment extends Fragment {
+public class DoneFragment extends Fragment {
 
 
 
-    public ThirdFragment() {
+    public DoneFragment() {
         // Required empty public constructor
     }
 
@@ -21,7 +21,7 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_third, container, false);
+        View view = inflater.inflate(R.layout.fragment_done, container, false);
         return view;
     }
 }
