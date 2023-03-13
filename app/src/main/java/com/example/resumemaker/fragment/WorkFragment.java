f844a6ca75c5541bfe94a2fa7a4a08d70bfd5187
Darshan Khunt <darshankhunt13@gmail.com>
2023-03-13 21:40:54 +0530

    Add card in workexp Tab

--- app/src/main/java/com/example/resumemaker/fragment/WorkFragment.java
+++ app/src/main/java/com/example/resumemaker/fragment/WorkFragment.java
@@ -1,13 +1,18 @@
 package com.example.resumemaker.fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.resumemaker.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class WorkFragment extends Fragment {
 
@@ -16,12 +21,19 @@ public class WorkFragment extends Fragment {
     public WorkFragment() {
         // Required empty public constructor
     }
-
+    EditText startDate0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_work, container, false);
+
+        startDate0 = view.findViewById(R.id.edStartDate0);
+
+
+
         return view;
     }
+
+
 }
