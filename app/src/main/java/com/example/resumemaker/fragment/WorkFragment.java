5607e421a8476b35489007af24b952ba1355c11f
Darshan Khunt <darshankhunt13@gmail.com>
2023-03-14 15:43:38 +0530

    Add multiple card in workexp Tab

--- app/src/main/java/com/example/resumemaker/fragment/WorkFragment.java
+++ app/src/main/java/com/example/resumemaker/fragment/WorkFragment.java
@@ -6,9 +6,13 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.resumemaker.R;
@@ -22,14 +26,41 @@ public class WorkFragment extends Fragment {
         // Required empty public constructor
     }
     EditText startDate0;
+    private LinearLayout lLayoutCard;
+    private int count = 0;
+    Button btnAddExp;
+    private CardView workCard0, workCard1, workCard2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_work, container, false);
 
-        startDate0 = view.findViewById(R.id.edStartDate0);
+        btnAddExp = view.findViewById(R.id.btnAddExp);
+        lLayoutCard = view.findViewById(R.id.lLayoutCard);
+        workCard0 = view.findViewById(R.id.WorkCard0);
+        workCard1 = view.findViewById(R.id.WorkCard1);
+        workCard2 = view.findViewById(R.id.WorkCard2);
+        lLayoutCard.setVisibility(View.VISIBLE);
 
+        btnAddExp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (count==0){
+                    workCard0.setVisibility(View.VISIBLE);
+                    count++;
+                } else if(count==1){
+                    workCard1.setVisibility(View.VISIBLE);
+                    count++;
+                } else if (count==2) {
+                    workCard2.setVisibility(View.VISIBLE);
+                    count++;
+                } else if (count==3) {
+                    Toast.makeText(getActivity(), "You can't insert more than 3 experience.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
         return view;
