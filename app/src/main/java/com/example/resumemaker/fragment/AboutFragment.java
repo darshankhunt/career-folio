ff998b0ff2de8a9732fb18f87083e4bd16f69206
Darshan Khunt <darshankhunt13@gmail.com>
2023-03-14 21:53:36 +0530

    Java Code in contact and about Frag.

--- app/src/main/java/com/example/resumemaker/fragment/AboutFragment.java
+++ app/src/main/java/com/example/resumemaker/fragment/AboutFragment.java
@@ -7,21 +7,68 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.resumemaker.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class AboutFragment extends Fragment {
-
-
-
     public AboutFragment() {
         // Required empty public constructor
     }
+
+    private TextInputLayout tilFirstName, tilLastName, tilProfession;
+    private Button btnNext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+
+        btnNext = view.findViewById(R.id.btnNext);
+        tilFirstName = view.findViewById(R.id.tilFirstName);
+        tilLastName = view.findViewById(R.id.tilLastName);
+        tilProfession = view.findViewById(R.id.tilProfession);
+
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String FirstName = tilFirstName.getEditText().getText().toString().trim();
+                String LastName = tilLastName.getEditText().getText().toString().trim();
+                String Profession = tilProfession.getEditText().getText().toString().trim();
+
+                if(FirstName.equals("")){
+                    tilFirstName.setError("Please Enter FirstName");
+                } else if (LastName.equals("")) {
+                    tilFirstName.setError("Please Enter LastName");
+                } else if (Profession.equals("")) {
+                    tilProfession.setError("Please Enter Profession");
+                }else {
+                    Toast.makeText(getActivity(), ""+FirstName+"|"+LastName+"|"+Profession, Toast.LENGTH_SHORT).show();
+                }
+
+//                if(!FirstName.equals("") && !LastName.equals("") && !Profession.equals("")){
+////                    String FirstName = tilFirstName.getEditText().getText().toString().trim();
+////                    String LastName = tilLastName.getEditText().getText().toString().trim();
+////                    String Profession = tilProfession.getEditText().getText().toString().trim();
+//
+//                    Toast.makeText(getActivity(), ""+FirstName+"|"+LastName+"|"+Profession, Toast.LENGTH_SHORT).show();
+//                }else {
+//                    Toast.makeText(getActivity(), "Hello World", Toast.LENGTH_SHORT).show();
+//                }
+            }
+        });
+
+
+
+
         return view;
     }
 }
--- app/src/main/java/com/example/resumemaker/fragment/ContactFragment.java
+++ app/src/main/java/com/example/resumemaker/fragment/ContactFragment.java
@@ -4,24 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.resumemaker.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class ContactFragment extends Fragment {
-
-
-
     public ContactFragment() {
         // Required empty public constructor
     }
+
+    private Button btnNext;
+    private TextInputLayout tilEmail, tilWebsite, tilCountry, tilMoNo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        btnNext = view.findViewById(R.id.btnNext);
+        tilEmail = view.findViewById(R.id.tilEmail);
+        tilMoNo = view.findViewById(R.id.tilMono);
+        tilWebsite = view.findViewById(R.id.tilWebsite);
+        tilCountry = view.findViewById(R.id.tilCountry);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String Email = tilEmail.getEditText().getText().toString().trim();
+                String Website = tilWebsite.getEditText().getText().toString().trim();
+                String Country = tilCountry.getEditText().getText().toString().trim();
+                String MoNO = tilMoNo.getEditText().getText().toString().trim();
+
+            }
+        });
+
+
+
         return view;
     }
 }
