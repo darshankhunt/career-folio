877b6c960631ca1e8af291dcd3014c2c2f127687
Darshan Khunt <darshankhunt13@gmail.com>
2023-03-19 11:05:49 +0530

    Modify Education Fragment

--- app/src/main/java/com/example/resumemaker/fragment/EducationFragment.java
+++ app/src/main/java/com/example/resumemaker/fragment/EducationFragment.java
@@ -11,47 +11,67 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.resumemaker.R;
+import com.example.resumemaker.databinding.FragmentEducationBinding;
+import com.example.resumemaker.databinding.FragmentWorkBinding;
 
-public class EducationFragment extends Fragment {
-    public EducationFragment() {
-        // Required empty public constructor
-    }
+public class EducationFragment extends Fragment implements View.OnClickListener{
 
-
-    private Button btnAddEdu;
-    private int count = 0;
-    private CardView eduCard0, eduCard1;
+    private static int count = 0;
+    FragmentEducationBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_education, container, false);
+//        View view = inflater.inflate(R.layout.fragment_education, container, false);
+        binding = FragmentEducationBinding.inflate(inflater,container,false);
 
 
-        btnAddEdu = view.findViewById(R.id.btnAddEdu);
-        eduCard0 = view.findViewById(R.id.EduCard0);
-        eduCard1 = view.findViewById(R.id.EduCard1);
+        binding.btnAddEdu.setOnClickListener(this);
+        binding.btnRemoveCard0.setOnClickListener(this);
+        binding.btnRemoveCard1.setOnClickListener(this);
+        binding.btnRemoveCard2.setOnClickListener(this);
 
 
-        btnAddEdu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+
+        return binding.getRoot();
+    }
+
+    public void removeCard(CardView workCard){
+        count--;
+        workCard.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnAddEdu:
                 if (count==0){
-                    eduCard0.setVisibility(View.VISIBLE);
+                    binding.EduCard0.setVisibility(View.VISIBLE);
                     count++;
                 } else if(count==1){
-                    eduCard1.setVisibility(View.VISIBLE);
+                    binding.EduCard0.setVisibility(View.VISIBLE);
+                    binding.EduCard1.setVisibility(View.VISIBLE);
                     count++;
                 } else if (count==2) {
-                    Toast.makeText(getActivity(), "You can't insert more than 2 Education.", Toast.LENGTH_SHORT).show();
+                    binding.EduCard0.setVisibility(View.VISIBLE);
+                    binding.EduCard1.setVisibility(View.VISIBLE);
+                    binding.EduCard2.setVisibility(View.VISIBLE);
+                    count++;
+                } else if (count>=3) {
+                    Toast.makeText(getActivity(), "You can't insert more than 3 Education.", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-
-
-
-
-        return view;
+                break;
+            case R.id.btnRemoveCard0:
+                removeCard(binding.EduCard0);
+                break;
+            case R.id.btnRemoveCard1:
+                removeCard(binding.EduCard1);
+                break;
+            case R.id.btnRemoveCard2:
+                removeCard(binding.EduCard2);
+                break;
+        }
     }
 }
