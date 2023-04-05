89ec2acc1682fd65e29461a6cd2a7de73601a0e8
Darshan Khunt <darshankhunt13@gmail.com>
2023-04-05 19:08:04 +0530

    Add Card in resumeDone Frag.

--- app/src/main/java/com/example/resumemaker/fragment/DoneFragment.java
+++ app/src/main/java/com/example/resumemaker/fragment/DoneFragment.java
@@ -4,24 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.resumemaker.R;
+import com.example.resumemaker.adapter.ResumeTemplateAdapter;
+import com.example.resumemaker.databinding.FragmentDoneBinding;
 
 public class DoneFragment extends Fragment {
 
+    FragmentDoneBinding binding;
 
-
-    public DoneFragment() {
-        // Required empty public constructor
-    }
+    private int[] arrResumeTemplate = {R.drawable.ic_tab_contact,R.drawable.ic_tab_contact,
+            R.drawable.ic_tab_contact,R.drawable.ic_tab_contact,R.drawable.ic_tab_contact,R.drawable.ic_tab_contact};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_done, container, false);
-        return view;
+        binding = FragmentDoneBinding.inflate(inflater,container,false);
+
+
+        ResumeTemplateAdapter adapter = new ResumeTemplateAdapter(getActivity(),arrResumeTemplate);
+        binding.gridview.setAdapter(adapter);
+
+
+
+
+        return binding.getRoot();
     }
 }
